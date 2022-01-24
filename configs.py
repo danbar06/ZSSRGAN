fa418d3d2660b6f784e31df00effaf6787149715
Neta <shaulneta@gmail.com>
2022-01-25 00:42:34 +0200

    change path separator to builtin function

--- configs.py
+++ configs.py
@@ -57,8 +57,7 @@ class Config:
 
     def clean_file_name(self):
         """Retrieves the clean image file_name for saving purposes"""
-        path_separator = '\\' if os.name == 'nt' else '/'
-        self.conf.img_name = self.conf.input_image_path.split(path_separator)[-1].replace('ZSSR', '') \
+        self.conf.img_name = self.conf.input_image_path.split(os.sep)[-1].replace('ZSSR', '') \
             .replace('real', '').replace('__', '').split('_.')[0].split('.')[0]
 
     def set_gpu_device(self):
